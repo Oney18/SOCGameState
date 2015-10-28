af43ffb075b2a61e21a5a82fd6dbd0f6d56a07c9
goldey17 <goldey17@up.edu>
2015-10-28 09:56:16 -0700

    Finsihed updating building and road classes. Added tile class

--- app/src/main/java/edu/up/cs301/soc/Building.java
+++ app/src/main/java/edu/up/cs301/soc/Building.java
@@ -15,20 +15,32 @@ public class Building {
     private int typeOfBuilding;
     private int [] adjacentRoads;
     private int [] adjacentTiles;
+
+    //Variables to eliminate use of random integers
     public static final int EMPTY = -1;
     public static final int SETTLEMENT = 0;
     public static final int CITY = 1;
     public static final int TOTAL_NUMBER_OF_BUILDING_SPOTS = 54;
     public static final int SMALLEST_NUMBER_OF_BUILDING_SPOTS = 0;
+    public static final int TOTAL_NUMBER_OF_TILE_SPOTS = 19;
+    public static final int SMALLEST_NUMBER_OF_TILE_SPOTS = 0;
+    public static final int TOTAL_NUMBER_OF_ROAD_SPOTS = 72;
+    public static final int SMALLEST_NUMBER_OF_ROAD_SPOTS = 0;
     public static final int TOTAL_NUMBER_OF_PLAYERS = 4;
     public static final int SMALLEST_NUMBER_OF_PLAYERS = 0;
+    public static final int TOTAL_NUMBER_TILES_ADJACENT_TO_A_BUILDING = 3;
+    public static final int SMALLEST_NUMBER_TILES_ADJACENT_TO_A_BUILDING = 0;
+    public static final int TOTAL_NUMBER_ROADS_ADJACENT_TO_A_BUILDING = 3;
+    public static final int SMALLEST_NUMBER_ROADS_ADJACENT_TO_A_BUILDING = 0;
 
     //Constructor
-    public Building (int newNumber){
+    public Building (int newNumber, int[] newAdjacentRoads, int[] newAdjacentTiles){
         setNumber(newNumber);
         isEmpty = true;
         player = EMPTY;
         typeOfBuilding = EMPTY;
+        adjacentRoads = newAdjacentRoads;
+        adjacentTiles = newAdjacentTiles;
     }
 
     //Method to return the building number
@@ -50,6 +62,17 @@ public class Building {
     public int getTypeOfBuilding(){
         return typeOfBuilding;
     }
+
+    //Method to return the array of adjacent roads
+    public int[] getAdjacentRoads(){
+        return adjacentRoads;
+    }
+
+    //Method to return the array of adjacent buildings
+    public int[] getAdjacentTiles(){
+        return adjacentTiles;
+    }
+
 
     //Method to set the number of the building
     public void setNumber( int newNumber){
@@ -75,5 +98,41 @@ public class Building {
         if ( type == SETTLEMENT || type == CITY) {
             typeOfBuilding = type;
         }
+    }
+
+    //Method to set one of the roads that are adjacent
+    public void setAdjacentRoads ( int index, int number){
+        if (index < TOTAL_NUMBER_ROADS_ADJACENT_TO_A_BUILDING && index >= SMALLEST_NUMBER_ROADS_ADJACENT_TO_A_BUILDING && number < TOTAL_NUMBER_OF_ROAD_SPOTS && number >= SMALLEST_NUMBER_OF_ROAD_SPOTS){
+            adjacentRoads[index] = number;
+        }
+    }
+
+    //Method to set one of the buildings that are adjacent
+    public void setAdjacentTiles ( int index, int number){
+        if (index < TOTAL_NUMBER_TILES_ADJACENT_TO_A_BUILDING && index >=
+                SMALLEST_NUMBER_TILES_ADJACENT_TO_A_BUILDING && number < TOTAL_NUMBER_OF_TILE_SPOTS
+                && number >= SMALLEST_NUMBER_OF_TILE_SPOTS){
+            adjacentTiles[index] = number;
+        }
+    }
+
+    //Method to check if a given road is adjacent. Returns true if adjacent
+    public boolean isAdjacentRoad (int roadNumber){
+        for(int i = SMALLEST_NUMBER_ROADS_ADJACENT_TO_A_BUILDING; i < TOTAL_NUMBER_ROADS_ADJACENT_TO_A_BUILDING; i++){
+            if (adjacentRoads[i] == roadNumber){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Method to check if a given tile is adjacent. Returns true if adjacent
+    public boolean isAdjacentTile (int tileNumber){
+        for(int i = SMALLEST_NUMBER_TILES_ADJACENT_TO_A_BUILDING; i < TOTAL_NUMBER_TILES_ADJACENT_TO_A_BUILDING; i++){
+            if (adjacentTiles[i] == tileNumber){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- app/src/main/java/edu/up/cs301/soc/Road.java
+++ app/src/main/java/edu/up/cs301/soc/Road.java
@@ -14,18 +14,27 @@ public class Road {
     private int player;
     private int [] adjacentRoads;
     private int [] adjacentBuildings;
+
+    //Variables to eliminate use of random integers
     public static final int EMPTY = -1;
     public static final int TOTAL_NUMBER_OF_ROAD_SPOTS = 72;
     public static final int SMALLEST_NUMBER_OF_ROAD_SPOTS = 0;
     public static final int TOTAL_NUMBER_OF_PLAYERS = 4;
     public static final int SMALLEST_NUMBER_OF_PLAYERS = 0;
+    public static final int TOTAL_NUMBER_OF_BUILDING_SPOTS = 54;
+    public static final int SMALLEST_NUMBER_OF_BUILDING_SPOTS = 0;
+    public static final int TOTAL_NUMBER_ROADS_ADJACENT_TO_A_ROAD = 4;
+    public static final int SMALLEST_NUMBER_ROADS_ADJACENT_TO_A_ROAD = 0;
+    public static final int TOTAL_NUMBER_BUILDINGS_ADJACENT_TO_A_ROAD = 2;
+    public static final int SMALLEST_NUMBER_BUILDINGS_ADJACENT_TO_A_ROAD = 0;
 
     //Constructor
-    public Road (int newNumber, int[] newAdjacentRoads){
+    public Road (int newNumber, int[] newAdjacentRoads, int[] newAdjacentBuildings){
         setNumber(newNumber);
         isEmpty = true;
         player = EMPTY;
         adjacentRoads = newAdjacentRoads;
+        adjacentBuildings = newAdjacentBuildings;
     }
 
     //Method to return the road number
@@ -46,6 +55,11 @@ public class Road {
     //Method to return the array of adjacent roads
     public int[] getAdjacentRoads(){
         return adjacentRoads;
+    }
+
+    //Method to return the array of adjacent buildings
+    public int[] getAdjacentBuildings(){
+        return adjacentBuildings;
     }
 
     //Method to set the number of the road
@@ -69,15 +83,37 @@ public class Road {
 
     //Method to set one of the roads that are adjacent
     public void setAdjacentRoads ( int index, int number){
-        if (index <= 3 && index >= 0 && number < TOTAL_NUMBER_OF_ROAD_SPOTS && number >= SMALLEST_NUMBER_OF_ROAD_SPOTS){
+        if (index < TOTAL_NUMBER_ROADS_ADJACENT_TO_A_ROAD &&
+                index >= SMALLEST_NUMBER_ROADS_ADJACENT_TO_A_ROAD &&
+                number < TOTAL_NUMBER_OF_ROAD_SPOTS && number >= SMALLEST_NUMBER_OF_ROAD_SPOTS){
             adjacentRoads[index] = number;
         }
     }
 
+    //Method to set one of the buildings that are adjacent
+    public void setAdjacentBuildings ( int index, int number){
+        if (index < TOTAL_NUMBER_BUILDINGS_ADJACENT_TO_A_ROAD &&
+                index >= SMALLEST_NUMBER_BUILDINGS_ADJACENT_TO_A_ROAD &&
+                number < TOTAL_NUMBER_OF_BUILDING_SPOTS &&
+                number >= SMALLEST_NUMBER_OF_BUILDING_SPOTS){
+            adjacentBuildings[index] = number;
+        }
+    }
+
     //Method to check if a given road is adjacent. Returns true if adjacent
-    public boolean isAdjacent (int roadNumber){
-        for(int i = 0; i < 4; i++){
+    public boolean isAdjacentRoad (int roadNumber){
+        for(int i = SMALLEST_NUMBER_ROADS_ADJACENT_TO_A_ROAD; i < TOTAL_NUMBER_ROADS_ADJACENT_TO_A_ROAD; i++){
             if (adjacentRoads[i] == roadNumber){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Method to check if a given building is adjacent. Returns true if adjacent
+    public boolean isAdjacentBuilding (int buildingNumber){
+        for(int i = SMALLEST_NUMBER_BUILDINGS_ADJACENT_TO_A_ROAD; i < TOTAL_NUMBER_BUILDINGS_ADJACENT_TO_A_ROAD; i++){
+            if (adjacentBuildings[i] == buildingNumber){
                 return true;
             }
         }
